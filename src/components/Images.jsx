4d3ff06e2bf2cf67486c7540b1ac95074903f273
Dushyna <olgavnv777@gmail.com>
2025-11-04 06:01:53 +0100

    refactoring SectionRight.jsx

---
+++ src/components/Images.jsx
@@ -0,0 +1,17 @@
+const Images = ({src}) => {
+    let style;
+    if (src==='/friend7.jpg') {
+        style = "col-sm-4 p-1 img1";
+    }else if(src==='/friend9.jpg') {
+        style = "col-sm-4 p-1 img2";
+    }else {
+        style = "col-sm-4 p-1";
+    }
+
+    return (
+        <img className={style} src={src} alt="Friend"/>
+
+    );
+};
+
+export default Images;
